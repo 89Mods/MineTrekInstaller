812f0fd60c6386bca322f23631fabb6fdd5c9464
89Mods <luca.horn@gmx.de>
2015-02-19 20:48:09 +0100

    Update InstallerFrame.java
    Replaced original download link with fake download link.

--- src/mine/trek/installer/InstallerFrame.java
+++ src/mine/trek/installer/InstallerFrame.java
@@ -3,7 +3,6 @@ package mine.trek.installer;
 import javax.swing.JFrame;
 
 public class InstallerFrame {
-//https://www.dropbox.com/s/3xo266hvx44lty2/minetrek-1.0.jar?dl=1
 	public static JFrame installerframe;
 	public static boolean looping = true;
 	/**
